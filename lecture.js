06e2e9aac3179182b3745c702c1fb8255141d98a
Rixvro <109919509+Rixvro@users.noreply.github.com>
2023-03-30 17:55:40 -0700

    Completed assignment

--- lecture.js
+++ lecture.js
@@ -7,16 +7,42 @@ Make sure to code-along during lecture.
 // 1. Link the script.js file from the HTML
 
 // Declaring varaibles use let or const, previously use var
-let text = "this is a sentence"
- // Changing the value of a varaible
+// let text = "this is a sentence"
+//  // Changing the value of a varaible
 
-    alert(text);
+//     alert(text);
 
-// 2. Console Log + Comments + Reserved Words + Camel Case + Naming Convention
-console.log(text);
-// 3. Creating Variables + Data Types
+// // 2. Console Log + Comments + Reserved Words + Camel Case + Naming Convention
+// console.log(text);
+// // 3. Creating Variables + Data Types
 
-// 4. Creating Functions (+ Event Listeners?)
+// // 4. Creating Functions (+ Event Listeners?)
+
+//     // Function statements:
+//     function nameOfFunction(){
+//         console.log('hello world');
+//     }
+//     function addition(a,b){
+//         console.log(a+b);
+//         return (a+b);
+//     }
+
+//     // Function expressions:
+//     let add = function addition2(a,b){
+//         let sum = a + b;
+//         console.log(sum);
+//     }
+//     // Invoking functions
+//     nameOfFunction();
+//     addition(2, 5);
+//     add(2, 6);
+    // Scope local and global
+
+    // Parameters as input
+
+    // Function Expression
+
+    // Arrow functions
 
 // 5. Strings
 
--- student.js
+++ student.js
@@ -18,5 +18,33 @@ let type = true;
     // 4. Assign one to the type String below:
 let text = "String";
 
-console.log(text);
-console.log(value);
+// console.log(text);
+// console.log(value);
+
+/*
+
+========== LESSON 2 - FUNCTIONS ==========
+
+*/
+
+// 1. Create a doubleNumber function statement that takes in 1 number parameter and returns double the value.
+function doubleNumber(a){
+    console.log(a*2);
+    return a * 2;
+}
+
+doubleNumber(3);
+
+
+// 2. Create a squareNumber function expression that takes in 1 number parameter and returns the square
+function squareNumber(a){
+    console.log(a**2);
+    return a ** 2;
+}
+
+squareNumber(3);
+
+// 3. Create a productNumber arrow function that takes in 2 number parameters and returns the product of the 2 numbers
+
+const productNumber = (a, b) => a * b;
+console.log(productNumber(3, 5));
